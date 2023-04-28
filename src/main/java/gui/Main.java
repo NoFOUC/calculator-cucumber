1a70589c14f557f3e8cb0299bcbec526dbb08c80
noafoucoux <Noa.foucoux@student.umons.ac.be>
2023-04-28 17:34:26 +0200

    Modification for getRessources for mvn install on command line

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -18,7 +18,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("./calculator.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/calculator.fxml"));
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
